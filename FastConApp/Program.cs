444af1af2c28325d5eaed83cbbda713b66df128e
Sakhawat hosen <sakhawat55cmt@outlook.com>
2020-02-03 18:20:21 +0600

    fast project

---
+++ FastConApp/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace FastConApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string fast = Console.ReadLine();
+            string second = Console.ReadLine();
+
+
+            string result = fast + second;
+            Console.WriteLine(result);
+            Console.ReadKey();
+        } 
+    }
+}
