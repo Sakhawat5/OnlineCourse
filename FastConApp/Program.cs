0c8eaba66ccfb83c9eca7ae546cd92598d005af2
Sakhawat hosen <sakhawat55cmt@outlook.com>
2020-02-03 19:13:05 +0600

    Static function done!

--- FastConApp/Program.cs
+++ FastConApp/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.ComponentModel;
 
 namespace FastConApp
 {
@@ -6,13 +7,33 @@ namespace FastConApp
     {
         static void Main(string[] args)
         {
-            string fast = Console.ReadLine();
-            string second = Console.ReadLine();
+            string fastNumStr = Console.ReadLine();
+            int fast = Convert.ToInt32(fastNumStr);
+            string secondNumStr = Console.ReadLine();
+            int second = Convert.ToInt32(secondNumStr);
 
+            int total = Add(fast, second);
+            Console.WriteLine(total);
 
-            string result = fast + second;
-            Console.WriteLine(result);
+            int sub = Subtract(fast, second);
+
+            Console.WriteLine(sub);
+            
+            //int result = fast + second;
+            //Console.WriteLine(result);
+
             Console.ReadKey();
-        } 
+        }
+
+        public static int Add(int fast, int second)
+        {
+            return fast + second;
+        }
+
+        public static int Subtract(int fast, int second)
+        {
+            return fast - second;
+        }
     }
+
 }
